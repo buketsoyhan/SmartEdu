a43bff29a95d8774e9f7bd94b3253d3bec328fdb
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-01-06 19:30:04 +0300

    Routing structure added to the project

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const pageRoute = require("./routes/pageRoute");
 
 const app = express();
 const port = 3000;
@@ -8,40 +9,8 @@ app.set("view engine", "ejs");
 
 //Middlewares -Statik dosyaların nerde olduğu gösterildi.
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.status(200).render("index", {
-    page_name: "index",
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.status(200).render("about", {
-    page_name: "about",
-  });
-});
-
-app.get("/courses", (req, res) => {
-  res.status(200).render("courses", {
-    page_name: "courses",
-  });
-});
-
-app.get("/dashboard", (req, res) => {
-  res.status(200).render("dashboard", {
-    page_name: "dashboard",
-  });
-});
-
-app.get("/contact", (req, res) => {
-  res.status(200).render("contact", {
-    page_name: "contact",
-  });
-});
-
-app.get("*", (req, res) => {
-  res.status(404).send("404 Page Not Found");
-});
+//Router dosyasında gerekli yönlendirmeler eklendi.
+app.use("/", pageRoute);
 
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
