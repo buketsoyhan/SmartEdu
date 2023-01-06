220f308b154e631c0c6d3397edb43f39b3b6c5fa
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-01-06 15:13:18 +0300

    Static files added and edited

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//EJS template engine kullanılcağı belirtildi.
+app.set("view engine", "ejs");
+
+//Middlewares -Statik dosyaların nerde olduğu gösterildi.
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.status(200).send("Main Page");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
 });
 
 app.get("*", (req, res) => {
-    res.status(404).send("404 Page Not Found");
-  });
+  res.status(404).send("404 Page Not Found");
+});
 
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
