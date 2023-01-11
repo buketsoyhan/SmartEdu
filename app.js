8824ddecaa5018d813eebb42a23afdcf1f2b13b4
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-01-11 04:10:14 +0300

    Added user login and logout

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require('path')
+const path = require("path");
+const session = require("express-session");
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
-const userRoute = require('./routes/userRoute')
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 const port = 3000;
@@ -12,8 +13,8 @@ const port = 3000;
 //DB bağlantısı
 mongoose
   .connect("mongodb://localhost/smartedu-db", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("DB connected successfully");
@@ -24,13 +25,28 @@ mongoose
 
 //EJS template engine kullanılcağı belirtildi.
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
+
+//Global Variable
+global.userIN = null;
+
 //Middlewares -Statik dosyaların nerde olduğu gösterildi.
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "my_keyboard_cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //Router dosyasında gerekli yönlendirmeler eklendi.
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
