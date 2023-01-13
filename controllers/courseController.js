d9fd02cfc8b47f1d2fb156c5c50d72b95c089d0e
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-01-13 03:07:47 +0300

    Added relation between course and teacher

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,8 +3,13 @@ const Category = require("../models/Category");
 
 exports.createCourse = async (req, res) => {
   try {
-    const course = await Course.create(req.body);
-    res.status(201).redirect('/courses')
+    const course = await Course.create({
+      name: req.body.name,
+      description: req.body.name,
+      category: req.body.category,
+      user: req.session.userID,
+    });
+    res.status(201).redirect("/courses");
   } catch (error) {
     res.status(400).json({
       status: "fail",
@@ -25,7 +30,7 @@ exports.getAllCourses = async (req, res) => {
       filter = { category: category._id };
     }
 
-    const courses = await Course.find(filter).sort('-createdAt');
+    const courses = await Course.find(filter).sort("-createdAt");
     const categories = await Category.find();
 
     res.status(200).render("courses", {
@@ -42,8 +47,10 @@ exports.getAllCourses = async (req, res) => {
 };
 exports.getCourse = async (req, res) => {
   try {
-    const course = await Course.findOne({ slug: req.params.slug });
-    res.status(200).render("course", {
+    const course = await Course.findOne({ slug: req.params.slug }).populate(
+      "user"
+    );
+    res.status(200).render("course-single", {
       course,
       page_name: "courses",
     });
